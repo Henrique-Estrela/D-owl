94a346cddce6a8f61476e19ff130c062a01e3708
Henrique Estrela <92654861+Henrique-Estrela@users.noreply.github.com>
2023-01-06 23:06:09 -0300

    up

--- index.php
+++ index.php
@@ -23,7 +23,9 @@
 
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script>
 
-    
+
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/iframe-resizer/4.3.2/iframeResizer.min.js"></script>
+
 
 </head>
 
@@ -43,16 +45,13 @@
                   <a href="" onmousemove="linha(5)" onmouseout="linha(6)" class="nav-link  l-3">Outros</a>
 
                 </li>
-                <li>
-                    
-                </li>
               
             </ul>
         </div>
     </nav>
     <main id="main">
             <div class="centralize-lado">
-                <ul class="t-5">
+                <ul class="t-4">
                     <li>
                         <span class="title" onmousemove="linha(2)" onmouseout="linha(1)">CONVERSOR YOUTUBE</span>
                     </li>
@@ -66,21 +65,33 @@
             <section class="centralize-lado">
 
                 <div class="caixa centralize-lado">
-                    <ul class="">
-                        <label class="text" >Insira o seu link do YouTube:</label>
-                        <li class="centralize-lado">
-                            <input placeholder="https://www.youtube.com/Exemplo" type="url" name="link" id="link"> 
-                        </li>
-                        <li class="centralize-lado">
-                            <input type="button" class="btn" value="Converter">
-                        </li>
-                    </ul>
-                    
+                    <form method="GET" action="https://convert2mp3s.com/api/button/{mp3}?url={https://www.youtube.com/watch?v=3wd3j9-piek}">
+                        <ul class="">
+                            <label class="text" >Insira o seu link do YouTube:</label>
+                            <li class="centralize-lado">
+                                <input placeholder="https://www.youtube.com/Exemplo" type="url" name="link" id="link" required> 
+                            </li>
+                            <li class="centralize-lado">
+                                <input type="submit" class="btn" value="Converter">
+                            </li>
+                        </ul>
+                    </form>
                 </div>
-
+                
+                
             </section>
-
     </main>
+    <section id="download" class="centralize-total t-5">
+
+        <iframe id="buttonApi " src="https://convert2mp3s.com/api/button/mp4?url=https://www.youtube.com/watch?v=3wd3j9-piek"
+        width="10%" height="10%"  allowtransparency="true" scrolling="no" style="border:none; background-color: #000; border: #ff2f1c solid 5px;  height: 20vh; border-radius: 3rem;  width: 60vw;background-color: rgb(255, 255, 255) !important;"></iframe>
+        
+    </section>
+    <iframe id="buttonApi " src="https://convert2mp3s.com/api/button/mp3?url=https://www.youtube.com/watch?v=3wd3j9-piek"
+        width="10%" height="10%"  allowtransparency="true" scrolling="no" style="border:none; background-color: #000; border: #ff2f1c solid 5px;  height: 20vh; border-radius: 3rem;  width: 60vw;background-color: rgb(255, 255, 255) !important;"></iframe>
+   
+<script>iFrameResize({ log: false, minHeight: 360 }, '#buttonApi')</script>
+
     <section id="propaganda">
         <a href="https://jovemnerd.com.br/nerdbunker/one-piece-film-red-novo-filme-animado-da-franquia-ganha-teaser-e-poster/"></a>
     </section>
